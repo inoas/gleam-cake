28169bc1791a863242cc2f993155592e9b7be399
inoas <mail@inoas.com>
2024-06-25 19:32:18 +0200

    create db

---
+++ test/gleam-cake-postgres-db-reset.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -eu
+
+echo
+
+echo Resetting database
+
+psql <<SQL
+SELECT 'CREATE DATABASE gleam_cake_test'
+WHERE NOT EXISTS (
+  SELECT FROM pg_database WHERE datname = 'gleam_cake_test'
+)\\gexec
+SQL
+
+# psql -v "ON_ERROR_STOP=1" -d gleam_cake_test <<SQL
+# DROP TABLE IF EXISTS cats;
+# CREATE TABLE cats (
+#   id SERIAL PRIMARY KEY,
+#   name VARCHAR(50) NOT NULL,
+#   is_cute boolean NOT NULL DEFAULT true,
+#   colors VARCHAR(50)[] NOT NULL,
+#   last_petted_at TIMESTAMP NOT NULL,
+#   birthday DATE NOT NULL
+# );
+# SQL
+
+echo Done
+echo
